c4cbb4417b9f38c885ee6e13a84283bf926d658f
Daniel Nugent <skatesf@gmail.com>
2015-03-13 21:57:43 -0700

    First commit

---
+++ app/src/main/java/com/photouploadapp/danielnugent/cameraphotouploadapp/Config.java
@@ -0,0 +1,13 @@
+package com.photouploadapp.danielnugent.cameraphotouploadapp;
+
+/**
+ * Created by danielnugent on 3/13/15.
+ */
+public class Config {
+
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://dnugent.nicewebsite.info/PhotoUploadApp/fileUpload.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android_File_Upload";
+}
